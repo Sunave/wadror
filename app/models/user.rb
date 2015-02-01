6c59c6d9a639f1044e2c6771528f903a90e820ac
Sunave <antti.suniala@gmail.com>
2015-02-01 16:06:46 +0200

    Resolve tasks from week 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ActiveRecord::Base
   include RatingAverage
 
+  has_secure_password
+
   validates :username, uniqueness: true,
             length: { in: 3..15 }
+  validates :password,
+            length: { minimum: 4 },
+            format: { with: /([A-Z].*[0-9]|[0-9].*[A-Z])/,
+                      message: "should have at least one number and one capital letter" }
 
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :beer_clubs, through: :memberships
 end

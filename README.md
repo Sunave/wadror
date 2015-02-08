618fc1ba3ef23d54ac818ee2ac7c7cdeb9cb4e2f
Sunave <antti.suniala@gmail.com>
2015-02-08 22:53:00 +0200

    Resolve tasks from week 4

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[![Build Status](https://travis-ci.org/Sunave/wadror.png)](https://travis-ci.org/Sunave/wadror) [![Code Climate](https://codeclimate.com/github/Sunave/wadror.png)](https://codeclimate.com/github/Sunave/wadror)
+
 Runs at http://sunave-ratebeer.herokuapp.com/
 
-[![Build Status](https://travis-ci.org/Sunave/wadror.png)](https://travis-ci.org/Sunave/wadror)
--- spec/features/beers_spec.rb
+++ spec/features/beers_spec.rb
@@ -6,24 +6,21 @@ describe 'Beers' do
   end
 
   it "creates beer which has non-empty name" do
+    sign_in(username:"Pekka", password:"Foobar1")
     visit new_beer_path
-
     fill_in('beer[name]', with:'Nonempty')
-
     expect{
       click_button('Create Beer')
     }.to change{Beer.count}.by(1)
   end
 
   it "doesn't let you create beer without name" do
+    sign_in(username:"Pekka", password:"Foobar1")
     visit new_beer_path
-
     fill_in('beer[name]', with: '')
-
     expect{
       click_button('Create Beer')
     }.to change{Beer.count}.by(0)
-
     expect(page).to have_content "Name can't be blank"
   end
 
